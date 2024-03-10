02b8f41a1cb1743d81c47b840adf840750ad62c2
David <davidcondori99@gmail.com>
2024-03-09 22:52:57 -0500

    Add HttpClientModule and update GifsService to use HttpClient

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,4 +1,7 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+
 
 @Injectable({
   providedIn: 'root'
@@ -6,8 +9,10 @@ import { Injectable } from '@angular/core';
 export class GifsService {
 
   private _tagHistory: string[] = [];
+  private apiKey: string = 'H1mfKBWL6iIP39EOVu823eI6PHFZsg8c'
+  private url: string = 'https://api.giphy.com/v1/gifs'
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   get tagHistorial() {
     return [...this._tagHistory];
@@ -27,6 +32,21 @@ export class GifsService {
   searchTag(tag: string): void {
     if (tag.length === 0) return;
     this.organizeHistory(tag);
+
+    const params = new HttpParams().set('api_key', this.apiKey).set('q',
+      tag).set('limit', '10');
+
+    this.http.get(`${this.url}/search`, { params })
+      .subscribe(response => {
+        console.log(response);
+      }
+      );
+
+    // fetch(`https://api.giphy.com/v1/gifs/search?api_key=${this.apiKey}&q=${tag}&limit=10`)
+    //   .then(response => response.json())
+    //   .then(data => {
+    //     console.log(data);
+    //   });
     console.log(this._tagHistory);
 
   }

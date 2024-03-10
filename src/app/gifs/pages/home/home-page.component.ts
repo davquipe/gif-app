e9b65588a5eba864ed1847df88cf22236906113d
David <davidcondori99@gmail.com>
2024-03-09 23:11:50 -0500

    Update card-list component to display gif titles

--- src/app/gifs/pages/home/home-page.component.ts
+++ src/app/gifs/pages/home/home-page.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { Datum, GifsService } from '../../services/gifs.service';
 
 @Component({
   selector: 'gifs-home-page',
   templateUrl: './home-page.component.html',
 })
 export class HomePageComponent {
+  constructor(private gifsService: GifsService) { }
 
+  get gifs(): Datum[] {
+    return this.gifsService.gifList;
+  }
 }

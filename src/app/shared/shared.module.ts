ec2c274a8b466bd99899d552b991f6837d797a3f
David <davidcondori99@gmail.com>
2024-03-09 20:42:45 -0500

    Update angular.json and app.component.ts, add GifsModule and SharedModule to app.module.ts, and change language to Spanish in index.html

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+
+
+@NgModule({
+  declarations: [
+    SidebarComponent,
+    FooterComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    SidebarComponent
+  ]
+})
+export class SharedModule { }
